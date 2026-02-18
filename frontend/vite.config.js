ba5ea7803fd8c60d3d0ade74b224ccce4953584a
Snap XD <harshpatankar21@gmail.com>
2026-02-18 17:06:09 +0530

    Step 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: "https://moviez-media.vercel.app",
+      routes: [
+        "/",
+        "/movies",
+        "/tv-series",
+        "/anime",
+        "/popular",
+        "/search",
+        "/profile",
+        "/watchlist",
+        "/request-movie",
+      ],
+    }),
+  ],
 });
